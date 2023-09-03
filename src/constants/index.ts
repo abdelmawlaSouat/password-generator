712a8b3c37809cf308b3822defb907c3d368b99b
asouat <abdel@lizy.be>
2023-09-03 17:50:17 +0200

    fix: responsive design

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_CHARACTER_LENGTH = 10;
 
-export const MAX_SLIDER_LENGTH = 20;
+export const MAX_SLIDER_LENGTH = 18;
 
 export const SYMBOLS = "~!@#$%^&*()_-+={[}}|:;\"'<,>.?/";
